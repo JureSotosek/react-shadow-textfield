5cc9306fdfe617a3632708b1ec08514595189b29
Jure Sotosek <jure.sotosek@gmail.com>
2018-09-26 13:27:10 +0200

    feat: Removed styled-components

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
 import React from 'react';
 
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  min-width: 100px;
-
-  box-sizing: border-box;
-  padding: 7px;
-  border-radius: 7px;
-  box-shadow: 0 5px 15px 0 rgba(37, 44, 97, 0.25);
-
-  display: flex;
-  flex-direction: row;
-`;
-
-const Label = styled.div`
-  margin-right: 5px;
-  margin-left: 5px;
-  font-size: 20px;
-`;
-
-const StyledInput = styled.input`
-  width: 100%;
-
-  border: 0 solid;
-  outline: none;
-
-  background-color: transparent;
-  font-size: 20px;
-`;
+import './index.css';
 
 class TextField extends React.Component {
   constructor() {
@@ -42,20 +14,13 @@ class TextField extends React.Component {
   }
 
   render() {
-    const {
-      className,
-      style,
-      labelLeft,
-      labelRight,
-      placeholder,
-      value,
-      onChange
-    } = this.props;
+    const { labelLeft, labelRight, placeholder, value, onChange } = this.props;
 
     return (
-      <Wrapper className={className} style={style}>
-        {labelLeft && <Label>{labelLeft}</Label>}
-        <StyledInput
+      <div className={'shadow_input_wrapper'} style={style}>
+        {labelLeft && <div className={'shadow_input_label'}>{labelLeft}</div>}
+        <input
+          className={'shadow_input_input'}
           type="text"
           placeholder={placeholder}
           value={value}
@@ -65,7 +30,7 @@ class TextField extends React.Component {
           }}
         />
         {labelRight && <Label>{labelRight}</Label>}
-      </Wrapper>
+      </div>
     );
   }
 }
