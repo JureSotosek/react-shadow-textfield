c8de754f5f1c21cdb179e2cdbdb2e749e17b7215
Jure Sotosek <jure.sotosek@gmail.com>
2018-09-26 13:31:40 +0200

    Revert "feat: Removed styled-components"
    This reverts commit 5cc9306fdfe617a3632708b1ec08514595189b29.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 
-import './index.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-width: 100px;
+
+  box-sizing: border-box;
+  padding: 7px;
+  border-radius: 7px;
+  box-shadow: 0 5px 15px 0 rgba(37, 44, 97, 0.25);
+
+  display: flex;
+  flex-direction: row;
+`;
+
+const Label = styled.div`
+  margin-right: 5px;
+  margin-left: 5px;
+  font-size: 20px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+
+  border: 0 solid;
+  outline: none;
+
+  background-color: transparent;
+  font-size: 20px;
+`;
 
 class TextField extends React.Component {
   constructor() {
@@ -14,13 +42,20 @@ class TextField extends React.Component {
   }
 
   render() {
-    const { labelLeft, labelRight, placeholder, value, onChange } = this.props;
+    const {
+      className,
+      style,
+      labelLeft,
+      labelRight,
+      placeholder,
+      value,
+      onChange
+    } = this.props;
 
     return (
-      <div className={'shadow_input_wrapper'} style={style}>
-        {labelLeft && <div className={'shadow_input_label'}>{labelLeft}</div>}
-        <input
-          className={'shadow_input_input'}
+      <Wrapper className={className} style={style}>
+        {labelLeft && <Label>{labelLeft}</Label>}
+        <StyledInput
           type="text"
           placeholder={placeholder}
           value={value}
@@ -30,7 +65,7 @@ class TextField extends React.Component {
           }}
         />
         {labelRight && <Label>{labelRight}</Label>}
-      </div>
+      </Wrapper>
     );
   }
 }
